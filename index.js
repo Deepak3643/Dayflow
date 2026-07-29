0367164de93f8823f1724f401995b7b6bdbe2936
Deepak Kumar <deepakkr3643@gmail.com>
2026-07-29 11:28:16 +0530

    prompt modified

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ console.log("Hello World");
 console.log("This is a sample code snippet from LocalRepo/index.js");
 
 const btn = document.getElementById("prompt-btn");
-    const output = document.getElementById("output");
-    btn.addEventListener("click", () => {
-      const userName = window.prompt("What is your name?", "");
-      output.textContent = "Hello, " + userName + "!";
-    });
+const output = document.getElementById("output");
+
+btn.addEventListener("click", () => {
+  output.textContent = `Hello, ${prompt("What is your name?")}!`;
+});
 
